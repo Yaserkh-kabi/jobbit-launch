c7074724d02da8278565aef28d710a549ce3bb70
Yasir Alkhayyat <yalkhayyat@kabi.ai>
2025-02-06 17:30:38 +0300

    fixesss

--- src/App.js
+++ src/App.js
@@ -1,20 +1,32 @@
 import './App.css';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Home from "./components/home";
-import Launch from "./components/launch";
+import React, { useRef, useState, useEffect } from 'react';
 
 function App() {
+  const videoRef = useRef(null);
+  const [launched, setLaunched] = useState(false);
+
+  const playVid = () => {
+    videoRef.current.play();
+  }
+
+  useEffect(()=>{
+    videoRef.current.currentTime = 1;
+  },[videoRef])
+
   return (
     <>
-      <Router>
-        <Routes>
-          <Route path="/" element={<Home/>} />
-          <Route path="/launch" element={<Launch/>} />
-          {/* <Route/> */}
-        </Routes>
-      </Router>
+      {!launched && <video
+        style={{cursor:"pointer"}}
+        className='full-page'
+        onEnded={()=>setLaunched(true)}
+        ref={videoRef}
+        onClick={playVid}
+      >
+        <source src="/vid.mp4" type="video/mp4" />
+      </video>}
+
+      {launched && <img className="full-page animated-vid" src="./bg-jobbit.png" alt="JOBBIT IS HERE !!!"/>}
     </>
   );
 }
-
 export default App;
